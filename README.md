7b2d3383a4ac6533f2e1a9be457f8c854206d533
Luke <dgm.bailey@outlook.com>
2022-04-24 09:09:50 +0300

    Updated kernel to Callanan

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # HyperOS
 
-HyperOS source tree - formerly Infinex project
+The HyperOS source tree - formerly Infinex project
 
 This is a custom operating system based on the kernel created by David Callanan (here: <strong>_<a href="https://github.com/davidcallanan/os-series/">CLICK ME</a>_</strong>)
+`Next planned feature:` A nice looking UI
--- src/impl/x86_64/boot/header.asm
+++ src/impl/x86_64/boot/header.asm
@@ -1,15 +1,16 @@
 section .multiboot_header
 header_start:
-    ; Magic number
-    dd 0xe85250d6 ; for multiboot2
-    ; Set our architecture
-    dd 0 ; For i386 protected mode
-    ; Get the header length
-    dd header_end - header_start
-    ; Have a checksum to make sure nothing is corrupted
-    dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-    ; End tags
-    dw 0
-    dw 0
-    dd 8
+	; magic number
+	dd 0xe85250d6 ; multiboot2
+	; architecture
+	dd 0 ; protected mode i386
+	; header length
+	dd header_end - header_start
+	; checksum
+	dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
+
+	; end tag
+	dw 0
+	dw 0
+	dd 8
 header_end:
